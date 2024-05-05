e8bb151dba31e7f69ab8ef4ed6ae747c11a455f2
LunaLunasi <LM230503@alumno.udb.edu.sv>
2024-05-05 11:22:48 -0600

    Agregar funcionalidad de Crear Reporte tomando en cuenta que la combobox "cmbPaciente" muestra los datos de los nombres de los pacientes ingresados anteriormente por el admin y usando el nombre seleccionado de esa combobox hace una consulta a la base para ver cuál es la cita más reciente registrada con ese nombre y extreae el Codcita y lo coloca como dato para el procedimiento almacenado c:

--- Sistema-Medico-2.0/PrototipoPED/Forms de Pantallazos/FrmCrearReporte.Designer.cs
+++ Sistema-Medico-2.0/PrototipoPED/Forms de Pantallazos/FrmCrearReporte.Designer.cs
@@ -35,8 +35,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtTemperatura = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtFrecuencia = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.txtEstatura = new System.Windows.Forms.TextBox();
             this.txtMotivo = new System.Windows.Forms.TextBox();
             this.txtDiagnostico = new System.Windows.Forms.TextBox();
@@ -135,31 +133,6 @@
             this.label4.Text = "Temperatura";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // txtFrecuencia
-            // 
-            this.txtFrecuencia.BackColor = System.Drawing.Color.White;
-            this.txtFrecuencia.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtFrecuencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFrecuencia.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(67)))), ((int)(((byte)(112)))));
-            this.txtFrecuencia.Location = new System.Drawing.Point(28, 250);
-            this.txtFrecuencia.Multiline = true;
-            this.txtFrecuencia.Name = "txtFrecuencia";
-            this.txtFrecuencia.Size = new System.Drawing.Size(107, 28);
-            this.txtFrecuencia.TabIndex = 51;
-            this.txtFrecuencia.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(67)))), ((int)(((byte)(112)))));
-            this.label5.Location = new System.Drawing.Point(28, 221);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(174, 20);
-            this.label5.TabIndex = 50;
-            this.label5.Text = "Frecuencia Cardiaca";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
             // txtEstatura
             // 
             this.txtEstatura.BackColor = System.Drawing.Color.White;
@@ -207,7 +180,7 @@
             this.txtPresion.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtPresion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPresion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(67)))), ((int)(((byte)(112)))));
-            this.txtPresion.Location = new System.Drawing.Point(300, 52);
+            this.txtPresion.Location = new System.Drawing.Point(28, 256);
             this.txtPresion.Multiline = true;
             this.txtPresion.Name = "txtPresion";
             this.txtPresion.Size = new System.Drawing.Size(107, 28);
@@ -219,7 +192,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(67)))), ((int)(((byte)(112)))));
-            this.label6.Location = new System.Drawing.Point(300, 23);
+            this.label6.Location = new System.Drawing.Point(28, 227);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(132, 20);
             this.label6.TabIndex = 56;
@@ -263,6 +236,7 @@
             this.btnGuardar.TabIndex = 61;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // btnBorrar
             // 
@@ -294,8 +268,6 @@
             this.Controls.Add(this.txtMotivo);
             this.Controls.Add(this.txtTemperatura);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtFrecuencia);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.txtEstatura);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPeso);
@@ -320,8 +292,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtTemperatura;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtFrecuencia;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtEstatura;
         private System.Windows.Forms.TextBox txtMotivo;
         private System.Windows.Forms.TextBox txtDiagnostico;
--- Sistema-Medico-2.0/PrototipoPED/Forms de Pantallazos/FrmCrearReporte.cs
+++ Sistema-Medico-2.0/PrototipoPED/Forms de Pantallazos/FrmCrearReporte.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -38,6 +39,72 @@ namespace PrototipoPED
         private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private string ConecStr = "data source=localhost; initial catalog=Clinica;" +
+       " persist security info=True; Integrated Security=SSPI; ";
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Obtener el nombre seleccionado de la combobox
+            string nombreCompleto = cmbPaciente.SelectedItem.ToString();
+            string[] nombres = nombreCompleto.Split(' ');
+
+            // Construir la consulta SQL para obtener el código de la última cita del paciente
+            string query = @"USE Clinica
+                        SELECT TOP 1 p.primerNombre, p.primerApellido, cm.codCita
+                        FROM administracion.pacientes p
+                        JOIN administracion.citasMedicas cm ON p.codPaciente = cm.codPaciente
+                        WHERE p.primerNombre = @PrimerNombre
+                        AND p.primerApellido = @PrimerApellido
+                        ORDER BY cm.fechaHora DESC";
+
+            // Ejecutar la consulta SQL
+            using (SqlConnection conn = new SqlConnection(ConecStr))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@PrimerNombre", nombres[0]);
+                cmd.Parameters.AddWithValue("@PrimerApellido", nombres[1]);
+
+                try
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        string codCita = reader.GetString(reader.GetOrdinal("codCita"));
+
+                        // Llamar al procedimiento almacenado con los datos ingresados y el código de cita obtenido
+                        using (SqlConnection conn2 = new SqlConnection(ConecStr))
+                        {
+                            SqlCommand cmd2 = new SqlCommand("medico.CrearReporte2p", conn2);
+                            cmd2.CommandType = CommandType.StoredProcedure;
+                            cmd2.Parameters.AddWithValue("@codcita", codCita);
+                            cmd2.Parameters.AddWithValue("@mot", txtMotivo.Text);
+                            cmd2.Parameters.AddWithValue("@diag", txtDiagnostico.Text);
+                            cmd2.Parameters.AddWithValue("@peso", decimal.Parse(txtPeso.Text));
+                            cmd2.Parameters.AddWithValue("@tal", decimal.Parse(txtEstatura.Text));
+                            cmd2.Parameters.AddWithValue("@temp", decimal.Parse(txtTemperatura.Text));
+                            cmd2.Parameters.AddWithValue("@presi", txtPresion.Text);
+
+                            conn2.Open();
+                            cmd2.ExecuteNonQuery();
+                            conn2.Close();
+
+                            // Mostrar un mensaje indicando que el reporte se ha ingresado correctamente
+                            MessageBox.Show("El reporte se ha ingresado correctamente.", "Reporte ingresado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontraron citas para el paciente seleccionado.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al obtener la cita del paciente: " + ex.Message);
+                }
+            }
         }
     }
 }
